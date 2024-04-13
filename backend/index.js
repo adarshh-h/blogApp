691014e34694b6f722e35440a4c8b69e5543795b
adarshh-h <145352114+adarshh-h@users.noreply.github.com>
2024-04-14 05:23:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ const secret = "asdfghjkzxcvbnmqwertyuiop";
 // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cors(
   {
-      origin: "https://blogweb-mern.netlify.app",
-    // origin: "https://blog-app-frontend-bay.vercel.app",
+      // origin: "https://blogweb-mern.netlify.app",
+     origin: "https://blog-app-frontend-bay.vercel.app",
      methods:["POST","GET"],
      credentials: true,
   }
